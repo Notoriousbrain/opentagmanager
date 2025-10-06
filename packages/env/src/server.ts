65b43271e849dd93f0b2f5de00db84f548810c81
Notoriousbrain <rohitsingh66604@gmail.com>
2025-10-06 14:21:54 +0530

    feat(env): add upstash url in @otm/env

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -6,12 +6,16 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    OTM_PROJECT_URL: z.string().min(1),
 
+    OTM_PROJECT_URL: z.string().min(1),
     OTM_DATABASE_URL: z.string().url(),
 
     OTM_UPSTASH_REDIS_REST_URL: z.string().url().optional(),
     OTM_UPSTASH_REDIS_REST_TOKEN: z.string().optional(),
+
+    OTM_REDIS_ENABLED: z.enum(["true", "false"]).default("false"),
+
+    OTM_LOCAL_REDIS_URL: z.string().url().optional(),
 
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
