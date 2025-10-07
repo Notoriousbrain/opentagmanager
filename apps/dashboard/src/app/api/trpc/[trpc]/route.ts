e02005f7cc4b40fa40fb8a45de39f078de596488
Notoriousbrain <rohitsingh66604@gmail.com>
2025-10-07 17:42:14 +0530

    feat(route): connect api route to dashboard

--- apps/dashboard/src/app/api/trpc/[trpc]/route.ts
+++ apps/dashboard/src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,14 @@
-import { appRouter } from "api/src/root";
+import { appRouter, createContext } from "@otm/api";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+export const runtime = "nodejs";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: async () => createContext({ headers: req.headers }),
   });
 
 export { handler as GET, handler as POST };
