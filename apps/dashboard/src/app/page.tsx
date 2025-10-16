ca2125d05019675b627b881f9cb75c0c2249bd56
Notoriousbrain <rohitsingh66604@gmail.com>
2025-10-16 15:43:00 +0530

    hero section

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,9 @@
+import MarketingPage from "./(marketing)/page";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center text-4xl font-medium p-4">
-      <h1>Welcome To OSS Tag</h1>
+    <div>
+      <MarketingPage />
     </div>
   );
 }
