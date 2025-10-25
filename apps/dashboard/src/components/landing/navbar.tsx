7e12fa5e4b405040e5d78b72cbfd82effc86d290
Notoriousbrain <rohitsingh66604@gmail.com>
2025-10-26 02:35:26 +0530

    star it

--- apps/dashboard/src/components/landing/navbar.tsx
+++ apps/dashboard/src/components/landing/navbar.tsx
@@ -2,6 +2,7 @@ import { Button } from "@otm/ui";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { BiStar } from "react-icons/bi";
 
 const NavbarLanding = () => {
   return (
@@ -15,11 +16,18 @@ const NavbarLanding = () => {
           className=""
         />
       </Link>
-      <Link href="/roadmap">
-        <Button className="font-bold cursor-pointer border border-gray-500 py-2 px-4 rounded-sm">
-          Roadmap
-        </Button>
-      </Link>
+      <div className="flex items-center gap-4">
+        <Link href="https://github.com/opentagmanager/osstag">
+          <Button className="font-bold cursor-pointer border border-gray-500 py-2 px-4 rounded-sm">
+           <BiStar size={16}/> Star
+          </Button>
+        </Link>
+        <Link href="/roadmap">
+          <Button className="font-bold cursor-pointer border border-gray-500 py-2 px-4 rounded-sm">
+            Roadmap
+          </Button>
+        </Link>
+      </div>
     </nav>
   );
 };
